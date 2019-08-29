02b2cdbc859853438ab5d76b92afe2c93ce463b6
balasss <47716534+balasss@users.noreply.github.com>
2019-08-29 17:57:01 +0530

    Create view.php

---
+++ view.php
@@ -0,0 +1,34 @@
+<html>
+<head> 
+<link rel="stylesheet" type="text/css" href="css/i.css">
+<link rel="stylesheet" type="text/css" href="css/asearch.css">
+</head>
+<title> </title>
+
+<body>
+<?php
+
+$link = mysqli_connect("localhost","root","","world") or die ("conn err:" . mysql_error());
+if(empty($id)){ 
+$action = $_REQUEST['action'];
+$id = $_REQUEST['id'];
+$sql = "SELECT * FROM city WHERE Name LIKE '$id%';";
+$result = $link->query($sql);
+//echo "Click More Detail of Cities </br>";
+//$count=mysqli_num_rows($result);
+//			echo "Total Records $count";
+//<a href='search.php?action=search&id=$data[Name]'>$data[Name]</a></td>";
+echo "<a href='index.php'> back to home page</a>";
+echo "<h3 align='center'>To Click More Detail of City </h3></br>";
+while($data = $result->fetch_array(MYSQLI_BOTH)){
+	echo "<table width='400' align='center' border=1>";
+	echo "<tr><td><center><a href='cview.php?action=search&id=$data[Name]'>$data[Name]</center></a></td>";
+	
+}
+}
+
+	echo "</tr>";
+echo "</table>";
+?>
+</body>
+</html>
