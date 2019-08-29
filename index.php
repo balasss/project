d35f213deafc4e0f10ea4e0e1d1920c8513f4ebe
balasss <47716534+balasss@users.noreply.github.com>
2019-08-29 17:53:39 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+<head> 
+<link rel="stylesheet" type="text/css" href="css/i.css">
+<link rel="stylesheet" type="text/css" href="css/asearch.css">
+</head>
+<title> </title>
+
+<body>
+
+<?php
+echo "<h1>City Search </h1>";
+echo "</br>";
+echo "<center>";
+echo "<form action='asearch.php' method='GET'>
+ <input type='text' name='name'><br>
+<input type='submit' name='submit' value='submit'>
+</form>";
+echo "<h2> search For City Alpabatic </h2>";
+echo "<table>";
+echo "<tr>";
+echo "<td><a href='view.php?action=view&id=A'>A</a></td>";
+echo "<td><a href='view.php?action=view&id=B'>B</a></td>";
+echo "<td><a href='view.php?action=view&id=C'>C</a></td>";
+
+echo "<td><a href='view.php?action=view&id=D'>D</a></td>";
+echo "<td><a href='view.php?action=view&id=E'>E</a></td>";
+echo "<td><a href='view.php?action=view&id=F'>F</a></td>";
+echo "<td><a href='view.php?action=view&id=G'>G</a></td>";
+echo "<td><a href='view.php?action=view&id=H'>H</a></td>";
+echo "<td><a href='view.php?action=view&id=I'>I</a></td>";
+echo "<td><a href='view.php?action=view&id=J'>J</a></td>";
+echo "<td><a href='view.php?action=view&id=K'>K</a></td>";
+echo "<td><a href='view.php?action=view&id=L'>L</a></td>";
+
+echo "<td><a href='view.php?action=view&id=M'>M</a></td>";
+echo "<td><a href='view.php?action=view&id=N'>N</a></td>";
+echo "<td><a href='view.php?action=view&id=O'>O</a></td>";
+echo "<td><a href='view.php?action=view&id=P'>P</a></td>";
+echo "<td><a href='view.php?action=view&id=Q'>Q</a></td>";
+echo "<td><a href='view.php?action=view&id=R'>R</a></td>";
+echo "<td><a href='view.php?action=view&id=S'>S</a></td>";
+echo "<td><a href='view.php?action=view&id=T'>T</a></td>";
+echo "<td><a href='view.php?action=view&id=U'>U</a></td>";
+
+echo "<td><a href='view.php?action=view&id=V'>V</a></td>";
+echo "<td><a href='view.php?action=view&id=W'>W</a></td>";
+echo "<td><a href='view.php?action=view&id=X'>X</a></td>";
+echo "<td><a href='view.php?action=view&id=Y'>Y</a></td>";
+echo "<td><a href='view.php?action=view&id=Z'>Z</a></td>";
+echo "</br>";
+echo "</table>";
+echo "</center>";
+?>
+
+</body>
+</html>
